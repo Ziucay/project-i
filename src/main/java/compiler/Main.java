0e463f53bcdfa8d2f710397157eb7157d9128387
Draeston <69845877+Draeston@users.noreply.github.com>
2022-02-09 20:13:27 +0300

    +fixed issue

--- src/main/java/compiler/Main.java
+++ src/main/java/compiler/Main.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.charset.Charset;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get("test.txt"));
         String source = new String(bytes, Charset.defaultCharset());
         Lexer lexer = new Lexer(source);
-        lexer.scanTokens();
-        System.out.println(lexer.toString());
+        List<Token> tokens = lexer.scanTokens();
+        System.out.println(tokens);
     }
 }
